b7c7ab64923e64fc4006652904ab84f0f5d8b879
VansTech <nicolepark@Nicoles-MacBook-Air.local>
2017-03-31 14:58:16 -0700

    Testing push

--- JumbleSolver/src/GameWindow.java
+++ JumbleSolver/src/GameWindow.java
@@ -15,6 +15,8 @@ public class GameWindow extends JFrame {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				//NICOLE WAS HERE (ignore testing git)
+				
 				try {
 					GameWindow frame = new GameWindow();
 					frame.setVisible(true);
