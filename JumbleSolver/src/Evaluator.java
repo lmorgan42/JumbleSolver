afa27d22ad5f6be2ee65171e9aacce6305cad87a
Nicole Park <heynikkipark@gmail.com>
2017-04-11 23:00:36 -0700

    fixed spelling

--- JumbleSolver/src/Evaluator.java
+++ JumbleSolver/src/Evaluator.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class Evaluater {
+public class Evaluator {
 	
-	public Evaluater(){
+	public Evaluator(){
 	}
 	
 	public ArrayList<String> getWords(ArrayList<String> list){ 
@@ -16,16 +16,17 @@ public class Evaluater {
 	}
 	//solves the string testing 
 	private String solve(String string) {
-		String s = "";
-		s = Alphabetizer.alph(string);
+		String s = Alphabetizer.alph(string);
 		HashMap hash = FileRead.getMap();
 		HashSet set = (HashSet)hash.get(s);
 		if (set == null){
 			return null;
 		}
+		//need to ask Hanson about last word solving to determine word
 		s = (String)set.iterator().next();
 		return s;
 	}
 	
 	
 }
+
