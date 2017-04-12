a9c8aec615d50ecf7261b83231a084cbc76c8006
Nicole Park <heynikkipark@gmail.com>
2017-04-12 13:24:08 -0700

    returns ArrayList for solver

--- JumbleSolver/src/Evaluator.java
+++ JumbleSolver/src/Evaluator.java
@@ -6,27 +6,31 @@ public class Evaluator {
 	
 	public Evaluator(){
 	}
-	
+		
 	public ArrayList<String> getWords(ArrayList<String> list){ 
 		ArrayList<String> words = new ArrayList<String>();
 		for (int i =0; i<list.size(); i++){
-			words.add(solve(list.get(i)));
+			words.addAll(solve(list.get(i)));
 		}
 		return words;
 	}
+
 	//solves the string testing 
-	private String solve(String string) {
-		String s = Alphabetizer.alph(string);
+	private ArrayList <String> solve(String string) {
+		ArrayList <String> s = new ArrayList<String>();
+		String temp = "";
+		temp = Alphabetizer.alph(string);
 		HashMap hash = FileRead.getMap();
-		HashSet set = (HashSet)hash.get(s);
+		HashSet set = (HashSet)hash.get(temp);
 		if (set == null){
 			return null;
 		}
-		//need to ask Hanson about last word solving to determine word
-		s = (String)set.iterator().next();
+		while (set.iterator().hasNext()==true){
+			temp = (String)set.iterator().next();
+			s.add(temp);
+		}
 		return s;
 	}
-	
 	
 }
 
